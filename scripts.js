d836c4bdb00feffc319c5ca7645b95b3580f7b90
ArgyroT <argirotseriktsoglou@hotmail.com>
2023-09-24 19:36:46 +0300

    finalization of the calc

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+const operators = ["/", "*", "-", "+"];
 const screen = document.getElementById("screen");
 const numberButtons = document.querySelectorAll(".number");
 
@@ -23,10 +24,39 @@ backButton.onclick = () => {
   }
 };
 
-const operatorButtons = document.querySelectorAll(".operator");
+const divideButton = document.getElementById("divide");
+divideButton.onclick = () => {
+  addOperatorToScreen("/");
+};
 
-operatorButtons.forEach((operatorButton) => {
-  operatorButton.onclick = () => {
-    //
-  };
-});
+const multiplyButton = document.getElementById("multiply");
+multiplyButton.onclick = () => {
+  addOperatorToScreen("*");
+};
+
+const minusButton = document.getElementById("minus");
+minusButton.onclick = () => {
+  addOperatorToScreen("-");
+};
+
+const plusButton = document.getElementById("plus");
+plusButton.onclick = () => {
+  addOperatorToScreen("+");
+};
+
+const equalsButton = document.getElementById("equals");
+equalsButton.onclick = () => {
+  // eval is bad and I am well aware of its risks when using it in production. How can I do it better?
+  screen.innerText = eval(screen.innerText);
+};
+
+function addOperatorToScreen(newOperator) {
+  operators.forEach((operator) => {
+    // if the last button clicked was any operator, remove it, so it can be replaced
+    if (screen.innerText[screen.innerText.length - 1] == operator) {
+      screen.innerText = screen.innerText.slice(0, -1);
+    }
+  });
+
+  screen.innerText += newOperator;
+}
